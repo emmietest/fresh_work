6f36c007eb721df9b94f6ddfeadee5fa8eac98e4
Elias <elias.smartest@gmail.com>
2026-03-11 08:23:44 +0100

    Updates

--- _next/static/A9ebAN3J22V5w12zI1k1T/_buildManifest.js
+++ _next/static/A9ebAN3J22V5w12zI1k1T/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/emmietest//_next/:path+",
+        "destination": "/emmietest/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
